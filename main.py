fbbea614873e9dc0477aed740c42f8335a890bb4
LEI-112966 <plays49@protonmail.com>
2025-11-25 02:20:24 +0000

    Implemented 2D version and completed sensing

--- main.py
+++ main.py
@@ -8,44 +8,25 @@ class Object:
         self.y = y
         self.name = name
 
-    def __str__(self):
-        return f'{self.x} {self.y} {self.name}'
+class Objective(Object):
+    def __init__(self, x, y):
+        super().__init__(x, y, '*')
 
-    def intersect(self):
-        return True
+    def __str__(self):
+        return f'Objective: {self.x} {self.y}'
+
+
+class Obstacle(Object):
+    def __init__(self, x, y):
+        super().__init__(x, y, '⬛')
+
+    def __str__(self):
+        return f'Obstacle: {self.x} {self.y}'
 
 
 class Agent(Object):
     def __init__(self, x, y, name):
         super().__init__(x, y, name)
-
-    def watch_environment(self, w):
-        vision_range = 5
-        up_list = []
-        for i in range(1, vision_range):
-            objs = w.get_at(self.x, self.y + i)
-            if objs is not None:
-                for obj in objs:
-                    up_list.append((obj.name, i))
-        right_list = []
-        for i in range(1, vision_range):
-            objs = w.get_at(self.x + i, self.y)
-            if objs is not None:
-                for obj in objs:
-                    right_list.append((obj.name, i))
-        down_list = []
-        for i in range(1, vision_range):
-            objs = w.get_at(self.x, self.y - i)
-            if objs is not None:
-                for obj in objs:
-                    down_list.append((obj.name, i))
-        left_list = []
-        for i in range(1, vision_range):
-            objs = w.get_at(self. x - i, self.y)
-            if objs is not None:
-                for obj in objs:
-                    left_list.append((obj.name, i))
-        return {"up": up_list, "down": down_list, "left": left_list, "right": right_list}
 
     def move_random(self, w):
         size = w.size
@@ -65,44 +46,45 @@ class Agent(Object):
                 w.move(self, self.x + 1, self.y)
                 break
 
+    def watch_direction(self, w, direction):
+        vision_range = 5
+        seen = []
+
+        if direction == "up":
+            modifier = (0, -1)
+        elif direction == "down":
+            modifier = (0, 1)
+        elif direction == "right":
+            modifier = (1, 0)
+        elif direction == "left":
+            modifier = (-1, 0)
+        else:
+            return None
+
+        for i in range(1, vision_range):
+            x, y = (self.x + (modifier[0]*i), self.y + (modifier[1]*i))
+            obj = w.get_at(x, y)
+            if obj is not None:
+                seen.append(obj.name)
+
+        if not seen:
+            return None
+        return seen
+
+    def watch_environment(self, w):
+        up_list = self.watch_direction(w, "up")
+        down_list = self.watch_direction(w, "down")
+        left_list = self.watch_direction(w, "left")
+        right_list = self.watch_direction(w, "right")
+        return {"up": up_list, "down": down_list, "left": left_list, "right": right_list}
+
     def printvision(self, vision, direction):
         if vision is not None:
             items = vision.get(direction)
             print(direction + ":")
             if items is not None:
                 for item in items:
-                    name = item[0]
-                    distance = item[1]
-                    print(name +  " at%2d spaces"%distance)
-
-
-
-
-    def __str__(self):
-        return f'Agent: {self.x} {self.y} {self.name}'
-
-
-class Objective(Object):
-    def __init__(self, x, y):
-        super().__init__(x, y, '*')
-
-    def __str__(self):
-        return f'Objective: {self.x} {self.y}'
-
-    def intersect(self):
-        return True
-
-
-class Obstacle(Object):
-    def __init__(self, x, y):
-        super().__init__(x, y, '⬛')
-
-    def __str__(self):
-        return f'Obstacle: {self.x} {self.y}'
-
-    def intersect(self):
-        return False
-
+                    print(item)
 
 class World:
     def __init__(self, size):
@@ -110,15 +92,12 @@ class World:
         self.size = size
         self.agents = []
 
-    def add_object(self, o):
-        target_pos = (o.x, o.y)
+    def add_object(self, obj):
+        target_pos = (obj.x, obj.y)
         if self.objects.get(target_pos) is None:
-            self.objects[target_pos] = []
-            self.objects[target_pos].append(o)
-        if self.objects.get(target_pos) is not None:
-            self.objects[target_pos].append(o)
-        if isinstance(o, Agent) and self.agents.count(o) == 0:
-            self.agents.append(o)
+            self.objects[target_pos] = obj
+        if isinstance(obj, Agent) and self.agents.count(obj) == 0:
+            self.agents.append(obj)
 
     def get_at(self, x, y):
         return self.objects.get((x, y))
@@ -126,52 +105,49 @@ class World:
     def move(self, agent, x, y):
         current_pos = (agent.x, agent.y)
         target_pos = (x, y)
-
         if current_pos == target_pos:
             return False
+        if self.objects.get(target_pos) is not None:
+            print("Place already occupied by " + self.objects.get(target_pos).name)
+            return False
 
-        objs_at_targ = self.objects.get(target_pos)
-        can_move = True
-        if objs_at_targ is not None:
-            for obj in objs_at_targ:
-                can_move = can_move and obj.intersect
+        self.objects.pop(current_pos)
+        agent.x, agent.y = target_pos
+        self.objects.update({target_pos: agent})
 
-        if can_move:
-            self.objects.get(current_pos).remove(agent)
-            agent.x = x
-            agent.y = y
-            self.add_object(agent)
-            return True
-        return False
+        return True
 
     def display(self):
-        matrix = [["." for x in range(self.size)] for y in range(self.size)]
-        for objs in self.objects.values():
-            for obj in objs:
-                if matrix[obj.x][obj.y] == ".":
-                    matrix[obj.x][obj.y] = obj.name
+        matrix = [["." for y in range(self.size)] for x in range(self.size)]
+        for obj in self.objects.values():
+            if matrix[obj.y][obj.x] == ".":
+                matrix[obj.y][obj.x] = obj.name
         for row in matrix:
             print(' '.join(row))
 
     def update(self):
         for agent in self.agents:
-            print("Agent " + agent.name + " moving")
-            agent.move_random(self)
             print("Agent " + agent.name + " sensing")
             vision = agent.watch_environment(self)
             agent.printvision(vision, "up")
             agent.printvision(vision, "down")
             agent.printvision(vision, "left")
             agent.printvision(vision, "right")
+            print("Agent " + agent.name + " moving")
+            agent.move_random(self)
 
 
 if __name__ == '__main__':
     print("Hello World")
-    world = World(7)
+    world = World(9)
     world.add_object(Agent(0, 1, "A"))
     world.add_object(Agent(0, 2, "B"))
+    world.add_object(Obstacle(0, 3))
+    world.add_object(Obstacle(1, 3))
+    world.add_object(Obstacle(2, 3))
+    world.add_object(Obstacle(3, 3))
     while True:
-        world.update()
         world.display()
+        world.update()
         time.sleep(1)
         print('\n')
