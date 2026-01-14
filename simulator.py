eb19ee94bcc733b259f044e17f376c7c75434edf
LEI-112966 <plays49@protonmail.com>
2026-01-14 23:18:18 +0000

    Minor optimization

--- simulator.py
+++ simulator.py
@@ -2,26 +2,25 @@ from world import Objective, Farol, Ambiente
 
 class Simulador:
     def __init__(self, listaAgente: list, ambiente: Ambiente):
-        self.listaAgentes = listaAgente
         self.ambiente = ambiente
+        self.listaAgentes = listaAgente
         for agent in self.listaAgentes:
             self.ambiente.add_object(agent)
+        self.objectives = []
+        for obj in ambiente.objects.values():
+            if isinstance(obj, Objective) or isinstance(obj, Farol):
+                self.objectives.append(obj)
 
     def listarAgentes(self):
         for agent in self.listaAgentes:
             print(agent)
 
     def executa(self):
-        objectives = []
-        for obj in self.ambiente.objects.values():
-            if isinstance(obj, Objective) or isinstance(obj, Farol):
-                objectives.append(obj)
         self.ambiente.atualizacao()
         for agent in self.listaAgentes:
             agent.executar(self.ambiente)
-
         win = False
-        for obj in objectives:
+        for obj in self.objectives:
             if obj.checkWin(self.ambiente):
                 win = True
         return win
